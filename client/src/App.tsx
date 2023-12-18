84b411c360a3be04f5f176c9c45956d546528a16
BurgHeart <154056603+BurgHeart@users.noreply.github.com>
2023-12-17 17:01:03 -0700

    storybook

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
-import { Route, Routes } from "react-router-dom";
-import { useGetWelcomeQuery } from "./api/hooks/useGetWelcomeQuery";
+import { FaHome } from 'react-icons/fa';
+import { Route, Routes } from 'react-router-dom';
+import { useGetWelcomeQuery } from './api/hooks/useGetWelcomeQuery';
 
 const App = () => {
   const { data: welcome, isLoading } = useGetWelcomeQuery()
@@ -8,7 +9,7 @@ const App = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<div className=" text-center text-3xl bg-red-500 p-1">{welcome && welcome.message}</div>} />
+      <Route path="/" element={<div className=" text-center text-3xl bg-red-500 p-1"><FaHome size={22} />{welcome && welcome.message}</div>} />
     </Routes>
   )
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import { config } from 'dotenv';
 import { connect, connection, Connection } from 'mongoose';
+import { error as err, log } from 'console';
 import { logger } from './middleware/loggerMiddleware';
+import cors from 'cors';
 import expresss, { Application, Request, Response, json, urlencoded } from 'express';
 
 // configure environment variables
@@ -12,11 +14,12 @@ const app: Application = expresss()
 // setup mongoose connection and connect to mongodb
 connect(process.env.MONGO_URI as string)
 const db: Connection = connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log(`connected to database: mongodb://${connection.name}`))
+db.on('error', (error) => err(error))
+db.once('open', () => log(`connected to database: mongodb://${connection.name}`))
 
 // middleware
 app.use(logger)
+app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 
@@ -27,4 +30,4 @@ app.get('/api/home', (req: Request, res: Response) => res.json({ message: 'Welco
 
 // start express server and listen for request
 const port: string|number = process.env.PORT || 3000
-app.listen(port, () => console.log(`listening for request: http://localhost:${port}/`))
+app.listen(port, () => log(`listening for request: http://localhost:${port}/`))
